fb74a5eb1bc57c3ed2e0c31af01c19b97fff7628
Seelammahesh <maheshmasina@gmail.com>
2023-10-27 22:06:57 +0530

    52nd commit

--- readme.txt
+++ readme.txt
@@ -3,3 +3,8 @@ Admin Inlines for foreign Keys
 NOTES :
 We can use admin inlines to modify  admin files in a django project
 In admin inlines we have seen stackedinlines and Tabularinlines
+
+video-52
+Tests in Django are automated checks to ensure your web application behaves correctly,
+ including unit tests for components, integration tests for interactions, and functional
+ tests from a user perspective.
--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,71 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Recipe,RecipeIngredients
+User=get_user_model()
 
-# Create your tests here.
+class UserTestcase(TestCase):
+    def setUp(self):
+        self.user_a = User.objects.create_user('trypythondjango',password='123654')
+
+    def test_user_pwd(self):
+        checked = self.user_a.check_password("123654")
+        self.assertTrue(checked)
+
+
+class RecipeTestCase(TestCase):
+    def setUp(self):
+        self.user_a = User.objects.create_user('trypythondjango',password='123654')
+        self.recipe_a=Recipe.objects.create(
+            name='GrilledChicken',
+            user=self.user_a
+        )
+        self.recipe_ingredient_a=RecipeIngredients.objects.create(
+            recipe=self.recipe_a,
+            name='Chicken',
+            quantity='5',
+            unit='rupees'
+        )
+
+
+    def test_user_count(self):
+        qs=User.objects.all()
+        self.assertEqual(qs.count(),1)
+
+    def test_user_recipe_reverse_count(self):
+        user=self.user_a
+        qs=user.recipe_set.all()
+        self.assertEqual(qs.count(),1)
+
+    def test_user_recipe_forward_count(self):
+        user=self.user_a
+        qs=Recipe.objects.filter(user=user)
+        self.assertEqual(qs.count(),1)
+
+    def test_recipe_ingredient_reverse_count(self):
+        recipe=self.recipe_a
+        qs = recipe.recipeingredients_set.all()
+        self.assertEqual(qs.count(), 1)
+
+    def test_recipe_ingredient_forward_count(self):
+        recipe = self.recipe_a
+        qs = RecipeIngredients.objects.filter(recipe=recipe)
+        self.assertEqual(qs.count(), 1)
+
+
+    def test_user_two_level_relation(self):
+        user=self.user_a
+        qs=RecipeIngredients.objects.filter(recipe__user=user)
+        self.assertEqual(qs.count(),1)
+
+    def test_user_two_level_reverse_relation(self):
+        user = self.user_a
+        recipeingredients_ids = list(user.recipe_set.all().values_list('recipeingredients__id', flat=True))
+        qs=RecipeIngredients.objects.filter(id__in=recipeingredients_ids)
+        self.assertEqual(qs.count(),1)
+        self.assertEqual(qs.count(),1)
+
+    def test_user_two_level_relation_via_recipe(self):
+        user = self.user_a
+        ids=user.recipe_set.all().values_list("id",flat=True)
+        qs=RecipeIngredients.objects.filter(recipe__id__in=ids)
+        self.assertEqual(qs.count(),1)
